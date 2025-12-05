4524a12c164cc1f89d4f5192cf18670a59376e0a
AiraFranco <adelosofranco@gmail.com>
2025-12-05 17:00:43 +0100

    Fixing os error 2

--- 18_pandas_duckdb/sql/load_sakila.sql
+++ 18_pandas_duckdb/sql/load_sakila.sql
@@ -2,4 +2,8 @@ INSTALL sqlite;
 
 LOAD sqlite;
 
-CALL sqlite_attach('data/sqlite-sakila.db'); -- had a problem with the path
+CALL sqlite_attach('18_pandas_duckdb/data/sqlite-sakila.db'); -- had a problem with the path
+
+/*
+Change: 'data/sqlite-sakila.db' To: '18_pandas_duckdb/data/sqlite-sakila.db''
+*/
