1fde65203bf5531300d3f927707a9fc4ddeb0c6d
AiraFranco <adelosofranco@gmail.com>
2025-12-06 00:16:44 +0100

    Fixed path error. Started with EDA using duckdb in python script from my raw sql queries

--- sql/load_sakila.sql
+++ sql/load_sakila.sql
@@ -2,7 +2,7 @@ INSTALL sqlite;
 
 LOAD sqlite;
 
-CALL sqlite_attach('18_pandas_duckdb/data/sqlite-sakila.db'); -- had a problem with the path
+CALL sqlite_attach('data/sqlite-sakila.db');
 
 /*
 Change: 'data/sqlite-sakila.db' To: '18_pandas_duckdb/data/sqlite-sakila.db''
